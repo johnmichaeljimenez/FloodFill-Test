22cc176c3c8aa038426c6208d3ba17664f47483f
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-01 17:13:28 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_cs;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Raylib.SetConfigFlags(ConfigFlags.FLAG_VSYNC_HINT);
+        Raylib.InitWindow(1366, 768, "Flood Fill Test");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.ClearBackground(Color.BLACK);
+
+            Raylib.BeginDrawing();
+
+            Raylib.DrawFPS(2, 2);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
