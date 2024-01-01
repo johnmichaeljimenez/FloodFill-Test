8a9ab61b79fb3b58295e2c2596989466c3604b0d
John <johnmichaeljimenez.thecmd@gmail.com>
2024-01-01 17:20:12 +0800

    Added grid drawing

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 
 public class Program
 {
+    static Color[,] Grid = new Color[8, 8];
+
     public static void Main(string[] args)
     {
         Raylib.SetConfigFlags(ConfigFlags.FLAG_VSYNC_HINT);
@@ -13,11 +15,28 @@ public class Program
 
             Raylib.BeginDrawing();
 
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    int size = 32;
+
+                    Raylib.DrawRectangle(x*size, y* size, size, size, Color.RED);
+                    Raylib.DrawRectangleLines(x * size, y * size, size, size, Color.WHITE);
+                }
+            }
+
+
             Raylib.DrawFPS(2, 2);
 
             Raylib.EndDrawing();
         }
 
         Raylib.CloseWindow();
+    }
+
+    static void FloodFill(int x, int y)
+    {
+
     }
 }
